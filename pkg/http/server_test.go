90edcc0f6ed35fc36221f15bdac144c0500936cb
szykol <szymonkolton@gmail.com>
2024-08-18 11:49:26 +0200

    test: Add more server test cases

--- pkg/http/server_test.go
+++ pkg/http/server_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/szykol/http/pkg/log"
 )
 
 type serverTestF struct {
@@ -17,6 +18,8 @@ func noOpHandler(ResponseWriter, *Request) {}
 
 func setupServerTest(t *testing.T) serverTestF {
 	ctx, cancel := context.WithCancel(context.Background())
+	logger := log.NewLogger(log.NewZapCfg())
+	ctx = log.WithContext(ctx, logger)
 
 	t.Cleanup(func() {
 		cancel()
@@ -39,11 +42,12 @@ func TestAddHandler(t *testing.T) {
 	})
 }
 
-func TestHandleNewConnection(t *testing.T) {
+func TestHandleRequest(t *testing.T) {
 	f := setupServerTest(t)
 
 	f.server.AddHandler("POST", "/test", func(w ResponseWriter, r *Request) {
-		w.Write([]byte("unit test"))
+		_, err := w.Write([]byte("unit test"))
+		assert.Nil(t, err, "Handler should not return error")
 	})
 
 	request := &Request{
@@ -58,4 +62,43 @@ func TestHandleNewConnection(t *testing.T) {
 	expectedResponse := []byte("HTTP/1.1 200 OK\r\nContent-Length: 9\r\nContent-Type: application/x-www-form-urlencoded\r\nConnection: Keep-Alive\r\nServer: go-simple-server\r\n\r\nunit test")
 
 	assert.Equal(t, expectedResponse, rd.Bytes())
+}
+
+func TestHandleRequestHandlerNotFound(t *testing.T) {
+	f := setupServerTest(t)
+
+	f.server.AddHandler("POST", "/test", func(w ResponseWriter, r *Request) {
+		_, err := w.Write([]byte("unit test"))
+		assert.Nil(t, err, "Handler should not return error")
+	})
+
+	request := &Request{
+		path:   "/nonexistent",
+		Method: "POST",
+	}
+
+	rd := &bytes.Buffer{}
+
+	f.server.handleRequest(f.ctx, request, rd)
+
+	assert.Contains(t, rd.String(), "HTTP/1.1 404 Not Found")
+}
+
+func TestHandleRequestInternalServerError(t *testing.T) {
+	f := setupServerTest(t)
+
+	f.server.AddHandler("POST", "/test", func(w ResponseWriter, r *Request) {
+		panic("unit test")
+	})
+
+	request := &Request{
+		path:   "/test",
+		Method: "POST",
+	}
+
+	rd := &bytes.Buffer{}
+
+	f.server.handleRequest(f.ctx, request, rd)
+
+	assert.Contains(t, rd.String(), "HTTP/1.1 500 Internal Server Error")
 }
